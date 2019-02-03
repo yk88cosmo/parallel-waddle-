889390ddb547b41a2c9ccbef5d3e32f876525f0b
yk88cosmo <47296494+yk88cosmo@users.noreply.github.com>
2019-02-03 22:34:26 +0800

    web applications

---
+++ YKtask1/script.js
@@ -0,0 +1,73 @@
+﻿//"b242508ece89277b9f5565d1841683c9"app"&units=imperial&APPID=b242508ece89277b9f5565d1841683c9"
+var weatherBaseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var weatherQueryParams = '&units=imperial&APPID=9af9987d0f66079a5baa5b00f7f58162';
+function createHTML(cityName, tempValue){
+	var bgClass;
+	if (Math.max(Math.round(tempValue-32)/1.8).toFixed(1) >= 50){
+		bgClass = 'redBg';
+		document.getElementById('validMsg').innerHTML = 'Very hot';//Math.max( Math.round(number * 10) / 10, 2.8 ).toFixed(1);
+	}
+	else if (Math.max(Math.round(tempValue-32)/1.8).toFixed(1) < 50 && Math.max(Math.round(tempValue-32)/1.8).toFixed(1) >= 40){
+		bgClass = 'orangeBg';
+		document.getElementById('validMsg').innerHTML = 'Hot';
+	}
+	else if (Math.max(Math.round(tempValue-32)/1.8).toFixed(1) < 40 && Math.max(Math.round(tempValue-32)/1.8).toFixed(1) >= 30){
+		bgClass = 'yellowBg';
+		document.getElementById('validMsg').innerHTML = 'Sunny';
+	}
+	else if (Math.max(Math.round(tempValue-32)/1.8).toFixed(1) < 30 && Math.max(Math.round(tempValue-32)/1.8).toFixed(1) >= 20){
+		bgClass = 'greenBg';
+		document.getElementById('validMsg').innerHTML = 'Normal';
+	}
+	else if (Math.max(Math.round(tempValue-32)/1.8).toFixed(1) < 20 && Math.max(Math.round(tempValue-32)/1.8).toFixed(1) >= 10){
+		bgClass = 'blueBg';
+		document.getElementById('validMsg').innerHTML = 'Rainy';
+	}
+	else{
+		bgClass = 'grayBg';
+		document.getElementById('validMsg').innerHTML = 'Heavy rains';
+	}
+	var htmlString ='<div class="setBorder ' + bgClass + '">' + '<div class="weatherCity">' + cityName + '</div>' + '<div class="weatherData">' + Math.max(Math.round(tempValue-32)/1.8).toFixed(1) + '°C</div>' + '</div>';
+	$('#weatherResults').prepend(htmlString);
+}
+var searchWeather = function(city){
+	var searchURL = weatherBaseURL + city + weatherQueryParams;
+	$.ajax({
+		url: searchURL,
+		type: 'GET',
+		dataType: 'json',
+		error: function(data){
+			console.log("We got problems");
+			console.log(data.status);
+			alert("Oops. Something went wrong...");
+		},
+		success: function(data){
+			console.log("Successful");
+			console.log(data);
+			if (data.cod === '404'){
+				alert("Error. Try again.");
+				return;
+			}
+			$("#query").val('');
+			var theCity = data.name || '????';
+			var theTemp = Math.round(data.main.temp) || 70;
+			createHTML(theCity, theTemp);
+		}
+	});
+};
+
+$(document).ready(function(){
+	console.log("Loaded!");
+	$("#search").on('click', function(){
+		console.log("Clicked search");
+		var newSearchTerm = $("#query").val();
+		console.log(newSearchTerm);
+		searchWeather(newSearchTerm);
+		$("#search").blur();
+	});
+	$('#query').on('keypress', function(e){
+		if (e.which == 13){
+			$("#search").trigger('click');
+		}
+	});
+});
---
+++ YKtask1/script_alt.js
@@ -0,0 +1,26 @@
+﻿function getWeatherInfo(){
+	console.log("Collecting...");
+	var theInputBox = document.getElementById('query');
+	var theInputValue = theInputBox.value;
+	console.log(theInputValue);
+	var weatherURL = 'http://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+	var apiID = "&APPID=YOUR-APP-ID";
+	var searchURL = weatherURL + theInputValue + appID;
+	
+	$.getJSON(searchURL, function(data){
+		console.log(data);
+		var temperature = "???";
+		if (data.cod === '404'){
+			alert("Try again.");
+			return;
+		}
+		console.log(data.main.temp);
+		temperature = data.main.temp;
+		var theResult = document.getElementById('weatherResults');
+		theResult.innerHTML = "The temperature in " + theInputValue + " is " + temperature;
+		}
+	);
+}
+
+var theButton = document.getElementById('search');
+theButton.onclick = getWeatherInfo;
